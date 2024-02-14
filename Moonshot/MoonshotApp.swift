0aa01a59d50da242b6a132e9da7c308d5d3683c4
Lucas Pennice <lucaspennice@gmail.com>
2024-02-14 08:31:53 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Lucas Pennice on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
